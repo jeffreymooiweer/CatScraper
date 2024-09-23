64dd00b4077559dbe83e19b8e9f16cac1884b8e3
Jeffrey Mooiweer <33760627+jeffreymooiweer@users.noreply.github.com>
2024-09-23 21:24:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,32 @@ const path = require('path');
 const fs = require('fs');
 const logger = require('./utils/logger');
 const uploadRoutes = require('./routes/upload');
+const i18n = require('i18n');
 
 const app = express();
 const upload = multer({ dest: 'uploads/' });
 
+i18n.configure({
+    locales: ['en', 'nl'],
+    directory: path.join(__dirname, 'public', 'locales'),
+    defaultLocale: 'nl',
+    queryParameter: 'lang',
+    autoReload: true,
+    updateFiles: false,
+    objectNotation: true
+});
+
+app.use(i18n.init);
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+// Middleware to set locale based on query parameter
+app.use((req, res, next) => {
+    if (req.query.lang) {
+        res.setLocale(req.query.lang);
+    }
+    next();
+});
 
 // Routes
 app.use('/', uploadRoutes);
